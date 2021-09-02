563aae95d863bd85faade86f8bc28d15510c0e6f
aeolus-23 <tonymaquiling@ucla.edu>
2021-09-02 10:46:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Movie Recommendation
+
+## Description
+Automatically retrieves the top movies for the day on IMDB and randomly selects one for you to watch tonight. 
+
+
---
+++ web_scrape.py
@@ -0,0 +1,41 @@
+#! python3
+# web_scrape.py - Scrapes IMDB list of movies to return a list of movies, dates,
+# urls, and synopses 
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+topRatedUrl = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+res = requests.get(topRatedUrl)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, 'html.parser')
+print("Start program. Downloading from " + topRatedUrl)
+
+top_250 = []
+movies = soup.select(".titleColumn a")    
+
+# Append a new dictionary for each movie to top_250 
+for movie in movies:
+    dic = {}
+    IMDB = "https://imdb.com"
+    print("Retrieving data for " + movie.text)
+    dic["title"] = movie.text
+    dic["url"] = IMDB + movie['href']
+    years = soup.select('.titleColumn span')
+    dic["year"] = years[0].text
+
+    # Retrieve synopsis from each movie's page
+    res = requests.get(dic["url"])
+    res.raise_for_status()
+    new_soup = BeautifulSoup(res.text, 'html.parser')
+
+    synopsis = new_soup.select(".GenresAndPlot__TextContainerBreakpointXL-cum89p-2")
+    dic["synopsis"] = synopsis[0].text
+
+    top_250.append(dic)
+
+# Write list to text file (temporary to check progress) using json string
+print("Writing list to text file...")
+with open("top250.txt", 'w') as f:
+    json.dump(top_250, f, indent=2)
